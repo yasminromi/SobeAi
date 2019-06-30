00c82a8dcee1e5172bd5390eba9a69d286deb92d
Felipe Rodrigues <filpgame@gmail.com>
2019-06-30 09:52:14 -0300

    merge issues

--- app/src/main/java/br/com/uberhack/testinho/ItineraryActivity.kt
+++ app/src/main/java/br/com/uberhack/testinho/ItineraryActivity.kt
@@ -2,11 +2,15 @@ package br.com.uberhack.testinho
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_itinerary.*
 
 class ItineraryActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_itinerary)
+        closeButton.setOnClickListener {
+
+        }
     }
 }
