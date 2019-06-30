f72e4ac50071d2491ee6891cf6e36ae76e03beb7
Felipe Rodrigues <filpgame@gmail.com>
2019-06-30 08:08:37 -0300

    added loading + route

--- app/src/main/java/br/com/uberhack/testinho/MapsActivity.kt
+++ app/src/main/java/br/com/uberhack/testinho/MapsActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.graphics.Color
 import android.location.Location
 import android.os.Bundle
+import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import android.view.View
@@ -15,7 +16,7 @@ import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.*
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.main.activity_maps.*
 
@@ -25,6 +26,13 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, RoutingListener {
     private var isMapReady = false
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private val locationListener = LocationListener()
+    private val polylines = arrayListOf<Polyline>()
+    private var lastLocation: Location? = null
+    private val COLORS = intArrayOf(
+        R.color.colorPrimary,
+        R.color.colorPrimaryDark,
+        R.color.colorAccent
+    )
 
     @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -61,7 +69,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, RoutingListener {
             .travelMode(AbstractRouting.TravelMode.DRIVING)
             .withListener(this)
             .waypoints(location, waypoint, destination)
-            .build();
+            .key(getString(R.string.google_maps_key))
+            .build()
         routing.execute()
     }
 
@@ -94,25 +103,80 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback, RoutingListener {
     }
 
     override fun onRoutingFailure(e: RouteException?) {
-        Toast.makeText(this, "RoutingFailure", Toast.LENGTH_SHORT).show()
+        Log.e("MapsActivity", e?.message, e)
     }
 
     override fun onRoutingStart() {
-        Toast.makeText(this, "RoutingStart", Toast.LENGTH_SHORT).show()
     }
 
-    override fun onRoutingSuccess(route: MutableList<Route>?, shortestRouteIndex: Int) {
-        Toast.makeText(this, "RoutingSuccess", Toast.LENGTH_SHORT).show()
+    override fun onRoutingSuccess(route: MutableList<Route>, shortestRouteIndex: Int) {
+        lastLocation?.let {
+            moveCameraWithMyLocation(it)
+        }
+
+        if (polylines.size > 0) {
+            for (poly in polylines) {
+                poly.remove()
+            }
+        }
+        polylines.clear()
+
+        //add route(s) to the map.
+        for (i in 0 until route.size) {
+
+            //In case of more than 5 alternative routes
+
+            val polyOptions = PolylineOptions()
+            polyOptions.color(resources.getColor(COLORS[i]))
+            polyOptions.width((10 + i * 3).toFloat())
+            polyOptions.addAll(route[i].points)
+            val polyline = googleMap.addPolyline(polyOptions)
+            polylines.add(polyline)
+
+            Toast.makeText(
+                applicationContext,
+                "Route " + (i + 1) + ": distance - " + route[i].distanceValue + ": duration - " + route.get(i).durationValue,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        var options = MarkerOptions()
+        lastLocation?.let {
+            options.position(LatLng(it.latitude, it.longitude))
+            options.icon(BitmapDescriptorFactory.fromResource(R.drawable.pin))
+            googleMap.addMarker(options)
+            return@let
+        }
+
+        options = MarkerOptions()
+        lastLocation?.let {
+            options.position(LatLng(-22.9946962, -43.2346334))
+            options.icon(BitmapDescriptorFactory.fromResource(R.drawable.pin))
+            googleMap.addMarker(options)
+            return@let
+        }
+        options = MarkerOptions()
+        options.position(LatLng(-22.9953358, -43.2429147))
+        options.icon(BitmapDescriptorFactory.fromResource(R.drawable.pin))
+        googleMap.addMarker(options)
+
+        val r = Runnable {
+            progressBar.visibility = View.GONE
+            overlay.visibility = View.GONE
+        }
+        Handler().postDelayed(r, 2000)
     }
 
     override fun onRoutingCancelled() {
-        Toast.makeText(this, "RoutingCancelled", Toast.LENGTH_SHORT).show()
+
     }
 
     inner class LocationListener : LocationCallback() {
         override fun onLocationResult(location: LocationResult?) {
             location?.lastLocation?.let {
+                this@MapsActivity.lastLocation = it
                 moveCameraWithMyLocation(it)
+                buildRoute(LatLng(it.latitude, it.longitude))
             }
         }
     }
