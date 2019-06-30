193c453cc5c4a1de0c01de02b72aee89472482dd
Felipe Rodrigues <filpgame@gmail.com>
2019-06-30 13:08:46 -0300

    adds listener to forward button

--- app/src/main/java/br/com/uberhack/testinho/ItineraryActivity.kt
+++ app/src/main/java/br/com/uberhack/testinho/ItineraryActivity.kt
@@ -1,6 +1,7 @@
 package br.com.uberhack.testinho
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_itinerary.*
 
@@ -9,9 +10,12 @@ class ItineraryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_itinerary)
-        closeButton.setOnClickListener {
-            setResult(MapsActivity.RESULT_RETURN)
-            finish()
-        }
+        closeButton.setOnClickListener(::goBack)
+        forwardButton.setOnClickListener(::goBack)
+    }
+
+    fun goBack(v: View) {
+        setResult(MapsActivity.RESULT_RETURN)
+        finish()
     }
 }
